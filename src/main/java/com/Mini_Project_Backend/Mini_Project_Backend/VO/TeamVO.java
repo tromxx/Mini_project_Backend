926da9b69b5ac32714274b83458876b1e7eb4e17
husik927@gmail.com <husik927@gmail.com>
2023-05-01 21:48:24 +0900

    TeamVO_Commit

--- src/main/java/com/Mini_Project_Backend/Mini_Project_Backend/VO/TeamVO.java
+++ src/main/java/com/Mini_Project_Backend/Mini_Project_Backend/VO/TeamVO.java
@@ -1,12 +1,14 @@
 package com.Mini_Project_Backend.Mini_Project_Backend.VO;
 import lombok.Setter;
-import lombok.Getter
+import lombok.Getter;
 
 @Setter
 @Getter
 
-public class Team {
+public class TeamVO {
     private String team_Name;
-    private int x;
+    private int team_Win;
+    private int team_Lose;
+    private int team_Win_Ratio;
 
 }
