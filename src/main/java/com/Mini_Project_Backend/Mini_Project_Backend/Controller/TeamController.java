4d7193ecad6fcf4066649fad8cdc3027b616372c
Junhyuk Kim <121940381+kimchoker@users.noreply.github.com>
2023-05-09 21:31:23 +0900

    수정

--- src/main/java/com/Mini_Project_Backend/Mini_Project_Backend/Controller/TeamController.java
+++ src/main/java/com/Mini_Project_Backend/Mini_Project_Backend/Controller/TeamController.java
@@ -1,19 +1,19 @@
-//package com.Mini_Project_Backend.Mini_Project_Backend.Controller;
-//import com.Mini_Project_Backend.Mini_Project_Backend.DAO.TeamDAO;
-//import com.Mini_Project_Backend.Mini_Project_Backend.VO.TeamVO;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.*;
-//import java.util.List;
-//
-//@CrossOrigin(origins = "http://localhost:3000")
-//@RestController
-//public class TeamController {
-//
-//    @GetMapping("/")
-//    public ResponseEntity<List<TeamVO>> getTeamInfo(@RequestParam String team){
-//        TeamDAO dao = new TeamDAO();
-//        List<TeamVO> list = dao.getTeamRanking();
-//        return new ResponseEntity<>(list, HttpStatus.OK);
-//    }
-//}
+package com.Mini_Project_Backend.Mini_Project_Backend.Controller;
+import com.Mini_Project_Backend.Mini_Project_Backend.DAO.TeamDAO;
+import com.Mini_Project_Backend.Mini_Project_Backend.VO.TeamVO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+public class TeamController {
+
+    @GetMapping("/")
+    public ResponseEntity<List<TeamVO>> getTeamInfo(@RequestParam String team){
+        TeamDAO dao = new TeamDAO();
+        List<TeamVO> list = dao.getTeamRanking();
+        return new ResponseEntity<>(list, HttpStatus.OK);
+    }
+}
