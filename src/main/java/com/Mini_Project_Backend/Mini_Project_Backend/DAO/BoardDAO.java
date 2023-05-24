d310ffcfdffed0066b80477e5181c3b06fa6eae4
husik927@gmail.com <husik927@gmail.com>
2023-05-24 20:52:21 +0900

    HomePlate Pagination 80%
    2023-05-24

--- src/main/java/com/Mini_Project_Backend/Mini_Project_Backend/DAO/BoardDAO.java
+++ src/main/java/com/Mini_Project_Backend/Mini_Project_Backend/DAO/BoardDAO.java
@@ -2,11 +2,10 @@ package com.Mini_Project_Backend.Mini_Project_Backend.DAO;
 
 import com.Mini_Project_Backend.Mini_Project_Backend.Common.Common;
 import com.Mini_Project_Backend.Mini_Project_Backend.VO.BoardVO;
-import com.Mini_Project_Backend.Mini_Project_Backend.VO.NewsVO;
 
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
+import java.sql.Date;
+import java.util.*;
 
 public class BoardDAO {
     private Connection conn = null;
@@ -14,69 +13,81 @@ public class BoardDAO {
     private ResultSet rs = null;
     private PreparedStatement pStmt = null;
 
-    // 보드 제목, 닉네임 조회 하기
-    public List<BoardVO> getShortBoard() {
-        List<BoardVO> list = new ArrayList<>();
+    // 보드 ALL 가죠오기 첫번쨰
+    public Map<String, Object> getTotalPageBoard() {
+        Map<String, Object> data = new HashMap<>();
         try {
-            String sql = "SELECT BOARD_NO, BOARD_TITLE, NICKNAME, BOARD_DATE FROM (SELECT BOARD_NO, BOARD_TITLE, NICKNAME, BOARD_DATE, ROW_NUMBER() OVER (ORDER BY BOARD_NO) AS rn FROM BOARD JOIN MEMBER ON BOARD.MEMBER_NO = MEMBER.MEMBER_NO) WHERE rn BETWEEN 1 AND 50";
+            String sql = "SELECT COUNT(*) FROM BOARD";
             conn = Common.getConnection();
             stmt = conn.createStatement();
             rs = stmt.executeQuery(sql);
-
-            while (rs.next()) {
-                int boardNo = rs.getInt("BOARD_NO");
-                String boardTitle = rs.getString("BOARD_TITLE");
-                String nickName = rs.getString("NICKNAME");
-                Date boardDate = rs.getDate("BOARD_DATE");
-
-                BoardVO boardVO = new BoardVO();
-                boardVO.setBoardNo(boardNo);
-                boardVO.setBoardTitle(boardTitle);
-                boardVO.setNickName(nickName);
-                boardVO.setBoardDate(boardDate);
-
-                list.add(boardVO);
-            }
+            rs.next();
+            int totalData = rs.getInt("COUNT(*)");
+            data.put("totalData", totalData);
             Common.close(rs);
             Common.close(stmt);
             Common.close(conn);
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return list;
-    }
-
-    // 보드 닉네임 다음 데이터 조회 하기
-    public List<BoardVO> getShortBoard(int boardno){
-        List<BoardVO> list = new ArrayList<>();
-        try{
-            String sql = "SELECT BOARD_NO, BOARD_TITLE, BOARD_IMG_URL, NICKNAME, BOARD_DATE FROM (SELECT T.*, ROWNUM AS RN FROM (SELECT B.*, M.NICKNAME FROM BOARD B INNER JOIN MEMBER M ON B.MEMBER_NO = M.MEMBER_NO WHERE B.BOARD_NO > ? ORDER BY B.BOARD_NO) T) WHERE RN BETWEEN 1 AND 50";
+        try {
+            String sql = "SELECT BOARD_NO, BOARD_TITLE, BOARD_DATE, NICKNAME FROM BOARD B INNER JOIN MEMBER M ON B.MEMBER_NO = B.MEMBER_NO WHERE ROWNUM <= 10";
             conn = Common.getConnection();
-            pStmt = conn.prepareStatement(sql);
-            pStmt.setInt(1, boardno);
-            rs = pStmt.executeQuery();
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery(sql);
+            List<Map<String, Object>> boardList = new ArrayList<>();
             while (rs.next()) {
-                int boardNo = rs.getInt("BOARD_NO");
-                String boardTitle = rs.getString("BOARD_TITLE");
+                Map<String, Object> boardData = new HashMap<>();
+                int board_No = rs.getInt("BOARD_NO");
+                String board_Title = rs.getString("BOARD_TITLE");
                 String nickName = rs.getString("NICKNAME");
-                Date boardDate = rs.getDate("BOARD_DATE");
-
-                BoardVO boardVO = new BoardVO();
-                boardVO.setBoardNo(boardNo);
-                boardVO.setBoardTitle(boardTitle);
-                boardVO.setNickName(nickName);
-                boardVO.setBoardDate(boardDate);
-
-                list.add(boardVO);
+                Date board_Date = rs.getDate("BOARD_DATE");
+                boardData.put("boardNo", board_No);
+                boardData.put("boardTitle", board_Title);
+                boardData.put("boardDate", board_Date);
+                boardData.put("nickName", nickName);
+                boardList.add(boardData);
             }
             Common.close(rs);
             Common.close(stmt);
             Common.close(conn);
-        }catch (Exception e){
+            System.out.println("finish");
+            data.put("data", boardList);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return data;
+    }
+
+    public List<BoardVO> getAllBoardByPage(int page){
+        List<BoardVO> list = new ArrayList<>();
+        int postPerPage = 10;
+        int endNum = page * postPerPage;
+        int startNum = endNum - (postPerPage-1);
+        try {
+            String sql = "SELECT BOARD_NO, BOARD_TITLE, NICKNAME, BOARD_DATE FROM (SELECT t.*, rownum AS r FROM (SELECT BOARD_NO, BOARD_TITLE, BOARD_DATE, NICKNAME FROM BOARD B INNER JOIN MEMBER M ON B.MEMBER_NO = B.MEMBER_NO) t WHERE rownum <= " + endNum + ") f WHERE r >=" + startNum;
+            conn = Common.getConnection();
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery(sql);
+            while (rs.next()) {
+                int board_No = rs.getInt("BOARD_NO");
+                String board_Title = rs.getString("BOARD_TITLE");
+                String nickName = rs.getString("NICKNAME");
+                Date board_Date = rs.getDate("BOARD_DATE");
+
+                BoardVO boardVO = new BoardVO();
+                boardVO.setBoardNo(board_No);
+                boardVO.setBoardTitle(board_Title);
+                boardVO.setNickName(nickName);
+                boardVO.setBoardDate(board_Date);
+                list.add(boardVO);
+            }
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return list;
     }
+    // 보드 번호로 정보 가죠오기
     public List<BoardVO> getLongBoard(int boardno){
         List<BoardVO> list = new ArrayList<>();
         try{
@@ -140,4 +151,8 @@ public class BoardDAO {
         }
         return list;
     }
+
+
+
+
 }
